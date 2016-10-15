e6aaac3648f29869b609abd0e6ca691b90c9d853
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-15 21:50:55 +0700

    Add scaffold FoodItem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
+  resources :food_items
   get '/contact-us',  to: 'contact_us#index'
   get '/menu',        to: 'menu#index'
+
+  root 'home#index'
 end
